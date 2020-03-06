19dbafa71cd61bb108a0cd817265b5c2bf332a49
uzapolsky <uzapolsky@gmail.com>
2020-03-07 01:01:50 +0300

    delete ignored files

--- .django_orm_2/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\shimmy\documents\github\devman_django_orm_2\.django_orm_2\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
