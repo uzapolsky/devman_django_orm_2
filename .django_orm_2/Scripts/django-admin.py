ccfe4554ff170a68a2fafc1bc882f9e61b65760b
uzapolsky <uzapolsky@gmail.com>
2020-03-07 00:55:57 +0300

    Add README.md

---
+++ .django_orm_2/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\shimmy\documents\github\devman_django_orm_2\.django_orm_2\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- project/settings.py
+++ project/settings.py
@@ -1,26 +1,33 @@
 import os
 
+from dotenv import load_dotenv
+from environs import Env
+
+load_dotenv()
+
+env = Env()
+env.read_env()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
         'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
-
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ROOT_URLCONF = "project.urls"
 
 ALLOWED_HOSTS = ['*']
 
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
